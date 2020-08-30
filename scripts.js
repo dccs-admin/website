3a9609f66012e51eee5ac0f7dc4744d98ed4f19c
chenxander <chenxander23@gmail.com>
2020-08-29 22:09:08 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ function sleep(ms) {
 function signUp(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
-    if (email == "") {
+    if (!email.exists) {
         window.alert("Please enter a valid email address and a password in the two boxes on the left and then press Sign Up.")
     }
 
